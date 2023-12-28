88b603d35ff4a359a2d565f436d69560cba652d1
Carlo Di Cicco <cdicicco2001@gmail.com>
2023-12-28 18:20:10 +0100

    È possibile effettuare la standardizzazione dei valori numerici all'interno di un dataframe.
    È stata aggiunta la classe "Standardizer" che con il suo metodo "standardize" effettua la standardizzazione dei dati numerici all'interno di un dataframe.

---
+++ standardizer.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+#La classe "Standardizer" ha il compito di effettuare il feature scaling di un dataframe utilizzando il metodo della standardizzazione
+#quest'ultima è stata preferita alla normalizzazazione perche non è influenzata dalla presenza di eventuali outliers
+class Standardizer:
+
+    #il metodo "standardize" riceve in input un dataframe e restituisce in output lo stesso dataframe con i valori scalati secondo la standardizzazione
+    #ogni valore viene prima sottratto della media e poi diviso per la deviazione standard
+    def standardize(self, df):
+        for col in df.columns:
+            if np.issubdtype(df[col].dtype, np.number):
+                mean_value = df[col].mean()
+                std_value = df[col].std()
+                df[col] = (df[col] - mean_value) / std_value
+
+        return df

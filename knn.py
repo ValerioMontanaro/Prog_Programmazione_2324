8ed5cdbea5bbe06f0c5d6683810a1e7f81330d31
Alessandro Gallo <2002alexgallo@gmail.com>
2024-01-03 12:43:00 +0100

    La function di test nella classe KNN per ora trova i K nearest neighbours in termini di distanza euclidea. MANCA la parte di classificazione dei punti.
    Per questa si dovrà memorizzare negli ndarray una colonna addizionale con l'id del dataframe di test che terrà traccia della categoria alla quale appartiene il punto.

--- knn.py
+++ knn.py
@@ -18,4 +18,22 @@ class KNN:
 
         return X_train, y_train, self.k
 
+    def test(self, df_test: pd.DataFrame, x_train: np.ndarray, y_train: np.ndarray, k: int) -> np.ndarray:
+        # in questo modo memorizzo in X_test sotto forma di numpy array i valori del dataframe df_test. Di questi
+        # valori si dovrà calcolare la distanza euclidea con i valori di x_train
 
+        # per far si che il calcolo sia coerente il numero di colonne dei due array deve essere uguale
+        X_test = df_test.iloc[:, :-1].values
+        if x_train.shape[1] != X_test.shape[1]:
+            raise ValueError("Il numero di colonne di X_train e X_test deve essere uguale")
+
+        # calcolo ora la distanza tra ogni punto che sta in X_test e ogni punto che sta in x_train
+        # in questo modo dist sarà un array di dimensione (numero di righe di X_test, numero di righe di x_train)
+        dist = None  # inizializzo dist a None per evitare un warning
+        for row in X_test:
+            dist = np.linalg.norm(x_train - row, axis=1)  # axis=1 indica che l'operazione di norma è per ogni RIGA
+
+        # ora dovrò considere i "k nearest neighbors" di ogni punto di X_test (ovvero considero ogni riga di dist)
+        # e la sorto in modo da avere in prima posizione la distanza più piccola e in ultima la distaza più grande
+        dist_sorted = sorted(dist, axis=1)  # axis=1 indica che l'operazione di sort è per ogni RIGA
+        k_near_neighbors = dist_sorted[:, :k]  # prendo solo i primi k valori di ogni riga

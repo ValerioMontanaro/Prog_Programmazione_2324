577b8fc4a7a6f27dae4d661fb77f8926ea2fddb3
Alessandro Gallo <2002alexgallo@gmail.com>
2024-01-29 11:28:58 +0100

    L'algoritmo del KNN è stato adeguato in quanto utilizza il 'code sample number' del dataframe originale. E' possibile utilizzare l'algoritmo utilizzando entrambi i metodi di validazione: holdout e stratified cross validation.

--- knn.py
+++ knn.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import splitterFactory
 
 
 class KNN:
@@ -10,51 +9,28 @@ class KNN:
         self.k = k
 
     def train(self, df_train: pd.DataFrame) -> (np.ndarray, np.ndarray, int):
-        # prima di tutto è importante salvare gli id delle righe per risalire ai valori delle features durante i test
-        # con reset_index() aggiugno una colonna alla fine con gli identificatori delle righe del dataframe
-        df_train_cp = df_train.copy()
-        df_train_cp['row_id'] = df_train.index
-
-        # ora però l'ultima colonna è il row_id quando in realtà per coerenza dovrebbe essere la colonna target, ovvero
-        # i valori y da stimare in fase di test
-        # per questo motivo sposto la colonna target in ultima posizione
-        cols = list(df_train_cp.columns)
-        # in questo modo sposto la colonna target in ultima posizione
-        # cols[:-2] arriva a prendere tutte le colonne tranne le ultime due
-        # cols[-1] prende l'ultima colonna che sarà il row_id
-        # cols[-2] prende la penultima colonna che sarà la colonna target
-        # riassemblo le colonne in questo modo e riscrivo il dataframe
-        cols = cols[:-2] + [cols[-1]] + [cols[-2]]
-        df_train_cp = df_train_cp[cols]
-
-        # in quest modo memorizzo in X_train sotto forma di numpy array i valori del dataframe df_train
-        # gli tolgo le ultime 2 colonne perchè sono il row_id e la colonna target
-        X_train = df_train_cp.iloc[:, :-2].values
-        X_train_id_y = df_train_cp.values  # prendo tutto il dataframe df_train
+        # in quest pezzo di codice memorizzo in X_train sotto forma di numpy array i valori del dataframe df_train
+        # gli tolgo l'ultima colonna perchè è la colonna target
+        X_train = df_train.iloc[:, :-1].values
+        # NB la prima colonna sarà il sample number che dovrà essere 'ignorato' nel momento in cui si calcola la
+        # calcola distanza euclidea tra i punti di train e i punti di test
+        X_train_y = df_train.values  # prendo tutto il dataframe df_train
 
         # in y_train memorizzo in un numpy array i valori della colonna target del dataframe df_train
-        y_train = df_train_cp.iloc[:, -1].values
-        return X_train, X_train_id_y, y_train, self.k
+        y_train = df_train.iloc[:, -1].values
+        return X_train, X_train_y, y_train, self.k
 
-    def test(self, df_test: pd.DataFrame, X_train: np.ndarray, X_train_id_y: np.ndarray, y_train: np.ndarray, k: int):
-        df_test_cp = df_test.copy()
-
+    def test(self, df_test: pd.DataFrame, X_train: np.ndarray, X_train_y: np.ndarray, y_train: np.ndarray, k: int):
         # exit_df avrà come numero di colonne le colonne del dataframe di test meno la colonna target ma più una colonna
         # che conterrà l'id del punto di test che sto considerando e più un'altra che conterrà la classe predetta
-        # exit_df = np.empty((df_test.shape[0], df_test.shape[1]))
         exit_df = None
 
-        # in questo modo memorizzo in X_test sotto forma di numpy array i valori del dataframe df_test. Di questi
-        # valori si dovrà calcolare la distanza euclidea con i valori di x_train
-        # NB anche in questo caso il test dovrà tenere conto del row identifier
-        df_test_cp['row_id'] = df_test_cp.index
-        cols = list(df_test_cp.columns)
-        cols = cols[:-2] + [cols[-1]] + [cols[-2]]
-        df_test_cp = df_test_cp[cols]
+        # in questo modo memorizzo in X_test sotto forma di numpy array i valori del dataframe df_test.
+        # gli tolgo l'ultima colonna perchè è la colonna target
 
-        # per far si che il calcolo sia coerente il numero di colonne dei due array deve essere uguale
-        X_test = df_test_cp.iloc[:, :-2].values
-        X_test_id_y = df_test_cp.values  # prendo tutte le colonne tranne l'ultima che è il row_id
+        X_test = df_test.iloc[:, :-1].values
+
+        # per far si che il calcolo sia coerente il numero di colonne dei due numpy array deve essere uguale
         if X_train.shape[1] != X_test.shape[1]:
             raise ValueError("Il numero di colonne di X_train e X_test deve essere uguale")
 
@@ -62,18 +38,18 @@ class KNN:
         # ogni punto singolamrnte di X_test.
         # in questo modo dist sarà un array con tante colonne quanti punti in X_train e una riga
         for row in X_test:
-            dist = np.linalg.norm(X_train - row, axis=1)  # axis=1 dice che l'operazione di norma è per ogni RIGA
+            dist = np.linalg.norm(X_train[:, 1:] - row[1:], axis=1) # axis=1 dice che l'operazione di norma è per ogni RIGA
             # voglio aggingere una riga a dist che contiene i valori della colonna target corrispondente alla riga di
             # X_train che sta in dist
             dist = np.vstack((dist, y_train))
-            # ora dist ha 2 righe e tante colonne quanti punti in X_train (ovvero tante colonne quanti punti in X_train)
+            # ora dist ha 2 righe e tante colonne quanti punti (ovvero righe) in X_train
 
             # voglio aggiungere un'altra riga a dist che contiene i valori dell'id corrispondente alla riga di X_train
-            # che è racchiuso nella penultima colonna di X_train_id_y
-            dist = np.vstack((dist, X_train_id_y[:, -2]))
+            # che è racchiuso nella PRIMA colonna di X_train_y
+            dist = np.vstack((dist, X_train_y[:, 0]))
             # ora dist ha 3 righe e tante colonne quanti punti ho in X_train
 
-            # voglio adesso sortare la prima riga di dist portando con se la seconda riga
+            # voglio adesso sortare la prima riga di dist portando con se la seconda riga e anche la terza
             # lo posso fare con la funzione np.argsort() che mi restituisce gli indici che ordinano una riga
             sorted_idx = np.argsort(dist[0])
             sorted_dist = dist[:, sorted_idx]
@@ -94,12 +70,6 @@ class KNN:
             row_to_be_added = np.concatenate((row, [most_frequent]))
             # NB a vstack devo passare una tupla perchè prende un positional argument
             exit_df = np.vstack((exit_df, row_to_be_added)) if exit_df is not None else np.array(row_to_be_added)
-
-        # finito il ciclio voglio aggiungere come colonna ad exit_df l'id del punto di X_test che sto considerando
-        # questo è conservato in X_test_id_y[:, -2]
-        # NB le dimensioni sono diverse quindi devo rendere la colonna aggiungibile a exit_df
-        exit_df = np.hstack((exit_df, X_test_id_y[:, -2].reshape(-1, 1))) # il -1 di reshape indica, in questo caso di
-        # trasofmrarlo in un array di quante righe è necessario per renderli compatibili
 
         return exit_df
 

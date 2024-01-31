43dd8f33f8493a59c92a09aa35910ab2741ecb0b
Carlo Di Cicco <cdicicco2001@gmail.com>
2024-01-31 18:22:35 +0100

    La classe "Reader_csv" implementa automaticamente nel metodo "parse" la pulizia di tutti i record duplicati del dataframe letto in input.

--- reader_csv.py
+++ reader_csv.py
@@ -7,6 +7,15 @@ class ReaderCSV(Reader):
     # implementazione concreta del metodo "parse" della classe astratta "Reader"
     # leggere il relativo commento per ulteriori informazioni sul metodo astratto "parse"
     # con la seguente implementazione concreta si definisce l'estensione "csv" del file di input
+    # il file letto in input come datframe viene prima ripulito dalle righe duplicate e
+    # successivamente viene reimpostato la colonna "Sample code number" come indice del dataframe
     def parse(self, filename):
-        df = pd.read_csv(filename, index_col="Sample code number")
+        df = pd.read_csv(filename)
+
+        # Rimuovere le righe duplicate
+        df = df.drop_duplicates()
+
+        # impostare 'Sample code number' come indice
+        df = df.set_index('Sample code number')
+
         return df

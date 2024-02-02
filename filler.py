9d01b58d1af7366ce2fc869bf42c3927f34a7005
Carlo Di Cicco <cdicicco2001@gmail.com>
2024-02-02 18:12:11 +0100

    È possibile testare la classe "Filler" e il suo metodo "fill" attraverso un codice coperto dalla variabile "__name__".

--- filler.py
+++ filler.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 """
 la classe "Filler" ha il compito di riempire i valori mancanti di un dataframe
@@ -17,3 +18,21 @@ class Filler:
                 df[col].fillna(mode_value, inplace=True)
 
         return df
+
+if __name__ == "__main__":
+    # Creazione di un DataFrame di esempio con valori mancanti
+    df_test = pd.DataFrame({
+        'A': [1, 2, np.nan, 5, 5, 6, 2],
+        'B': [np.nan, 2, 3, 4, 7, np.nan, 7],
+        'C': [np.nan, 1, np.nan, 1, 2, 3, 4]
+    })
+
+    print("DataFrame originale:")
+    print(df_test)
+
+    # Istanza della classe Filler e utilizzo del metodo fill
+    filler = Filler()
+    df_filled = filler.fill(df_test)
+
+    print("\nDataFrame dopo il riempimento dei valori mancanti:")
+    print(df_filled)
